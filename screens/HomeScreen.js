c34b3f9790775362a2b1f88888aba6bc13240287
Jimmy <jimmy.hotchkiss@gmail.com>
2022-03-06 16:36:25 -0800

    Added react-native-map and rendered map and marker.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,10 +5,15 @@ import NavOptions from '../components/NavOptions'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 // import Google api key
 import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux';
+// import our actions from our slices
+import { setDestination, setOrigin } from '../slices/navSlice';
+import { setUseProxies } from 'immer';
 // 'react-native-web' doesn't work, import directly from 'react-native'
 // import { SafeAreaView } from 'react-native-web'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -32,8 +37,22 @@ const HomeScreen = () => {
               fontSize: 18,
             }
           }} 
+          onPress={(data, details = null) => {
+            console.log(details.geometry.location)
+            dispatch(setOrigin({
+              location: details.geometry.location,
+              description: data.description
+            }))
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
           query={{
-            key: GOOGLE_MAPS_APIKEY
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
           }}
           nearbyPlacesAPI='GooglePlacesSearch'
           debounce={400}
